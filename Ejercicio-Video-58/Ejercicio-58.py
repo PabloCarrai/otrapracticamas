05f1fe15b7467b9b152b443fbddfa0864c615300
PabloCarrai <pablohernanjuan@hotmail.com>
2025-12-20 16:49:33 -0300

    va

---
+++ Ejercicio-Video-58/Ejercicio-58.py
@@ -0,0 +1,18 @@
+#   Labmda
+
+
+"""
+
+def sumar(n1,n2):
+    return n1+n2
+
+print(sumar(12,15))
+
+"""
+#   esto es la funcion lambda
+sumar = lambda n1, n2: n1 + n2
+print(sumar(45,14))
+misuma=sumar(33,44)
+print(misuma)
+elevarCuadrado=lambda numero:numero*numero
+print(elevarCuadrado(4))
